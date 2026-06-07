25a46e119f18c8ed2d5040c13526304521da46dc
PARIMALABALAMURUGAN <kparimala.cs@gmail.com>
2026-06-07 06:08:28 +0530

    Add files via upload

---
+++ multipleclass.py
@@ -0,0 +1,11 @@
+class multipleclass():
+
+    def Subfields():
+        print("Sub-fields in AI are:")
+
+    def OddEven():
+        num = int(input("Enter a number: "))
+        if num % 2 == 0:
+            print(num, "is Even Number")
+        else:
+            print(num, "is Odd Number")
